09e82715c156f25f0f74d89cf79a50f77600538e
peckerr <162071537+peckerr@users.noreply.github.com>
2025-11-27 20:31:50 +1100

    animal species sub-classes created, inheriting methods for sound making

---
+++ bird.py
@@ -0,0 +1,14 @@
+'''
+File: bird.py
+Description: A brief description of this Python module.
+Author: Thomas Cochrane
+ID: 110466784
+Username: COCTY007
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from animal import Animal
+
+class Bird(Animal):
+    def make_sound(self) -> str:
+        return f'{self.__name} makes a bird noise'
---
+++ mammal.py
@@ -0,0 +1,14 @@
+'''
+File: mammal.py
+Description: A brief description of this Python module.
+Author: Thomas Cochrane
+ID: 110466784
+Username: COCTY007
+This is my own work as defined by the University's Academic Integrity Policy.
+'''
+
+from animal import Animal
+
+class Mammal(Animal):
+    def make_sound(self) -> str:
+        return f'{self.__name} makes a mamilian noise.'
