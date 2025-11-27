05f368b21c57ba81fc1d51422ee99e33857b7438
peckerr <162071537+peckerr@users.noreply.github.com>
2025-11-27 20:35:34 +1100

    enclosure class created

--- enclosure.py
+++ enclosure.py
@@ -7,3 +7,14 @@ Username: COCTY007
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+class Enclosure:
+    def __init__(self, size: int, environment_type: str, animal_type: str):
+        if size < 0:
+            raise ValueError('size must be positive')
+
+        self.__size = size
+        self.__environment_type = environment_type
+        self.__animal_type = animal_type
+        self.__animals = []
+        self.__clean_level = 100
+
