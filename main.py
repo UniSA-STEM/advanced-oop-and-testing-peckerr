8d7ed0a894d687841194158c7639dc5bc567442a
peckerr <162071537+peckerr@users.noreply.github.com>
2025-11-28 20:38:25 +1100

    Beginining testing phase with main.py

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ Username: COCTY007
 This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
+from animal import Animal
+from mammal import Mammal
+from reptile import Reptile
+from bird import Bird
+
+from staff import Staff
+from zookeeper import Zookeeper
+from veterinarian import Veterinarian
+
+from enclosure import Enclosure
+from healthRecord import HealthRecord
+
